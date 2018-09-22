afc66fe83c5ac36b3fbdb95e2d57a1018e1363dd
Julio Quintana <jquintana1801@gmail.com>
2018-09-22 18:40:27 -0300

    core(webpack): Setting babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
