01f8f4da3847622a4e3a23cbced85cc21a7f7526
Julio Quintana <jquintana1801@gmail.com>
2018-09-25 18:21:19 -0300

    fix(form-save): Adjust form event to save and edit

--- src/domain/ListImagesSortable/FormEvents.js
+++ src/domain/ListImagesSortable/FormEvents.js
@@ -20,6 +20,34 @@ function readURL() {
 
 const error = error => {};
 
+const onCompleteNewItem = (uploadTask, database) => {
+  let count = typeof listItems !== 'undefined' ? listItems.querySelectorAll("li").length : 0;
+  uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+    let newItem = database.ref('items').push();
+
+    newItem.set({
+      downloadURL,
+      description: descriptionImg.value,
+      order: count
+    }).then(() => {
+      if(['null', 'undefined'].indexOf(typeof listItems) === -1){
+        let item = {id: newItem.key,downloadURL, description: descriptionImg.value, order: count};
+        listItems.appendChild(Item(CardElem(item), item));
+        showCountItem.innerHTML = `<small>${count+1} items listed.</small>`
+      } else {
+        loadItemsforList(rowMain, List, fb);
+      }
+
+      descriptionImg.value = '';
+      getImageFromPc.value = '';
+      idItem.value = '';
+      setDescription.setAttribute('style', 'display:none');
+      setImage.setAttribute('style', 'display:block');
+      alert('The new item was added in the bottom of list');
+    })
+  });
+} 
+
 export default function initFormEvents(fb) {
   let storage  = fb.storage().ref();
   let database = fb.db()
@@ -30,39 +58,19 @@ export default function initFormEvents(fb) {
   });
   
   btnSave.addEventListener('click', () => {
+    const isEdit = idItem.value.length !== 0;
     btnSave.disabled = true;
     let file = getImageFromPc.files[0];
     let uploadTask = storage.child(`items/${file.name}`).put(file);
 
-    uploadTask.on('state_changed', 
-    null,
-    null,
-    () => {
-      let count = typeof listItems !== 'undefined' ? listItems.querySelectorAll("li").length : 0;
-      uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-        let newItem = database.ref('items').push();
+    if(isEdit){
 
-        newItem.set({
-          downloadURL,
-          description: descriptionImg.value,
-          order: count
-        }).then(() => {
-          if(['null', 'undefined'].indexOf(typeof listItems) === -1){
-            let item = {id: newItem.key,downloadURL, description: descriptionImg.value, order: count};
-            listItems.appendChild(Item(CardElem(item), item));
-            showCountItem.innerHTML = `<small>${count+1} items listed.</small>`
-          } else {
-            loadItemsforList(rowMain, List, fb);
-          }
-
-          descriptionImg.value = '';
-          getImageFromPc.value = '';
-          setDescription.setAttribute('style', 'display:none');
-          setImage.setAttribute('style', 'display:block');
-          alert('The new item was added in the bottom of list');
-        })
-      });
-    });
+    } else {
+      uploadTask.on('state_changed', 
+      ()=>{},
+      e=>{console.log(e)},
+      onCompleteNewItem(uploadTask, database));
+    }
   });
 
   descriptionImg.addEventListener('keyup', () => {
